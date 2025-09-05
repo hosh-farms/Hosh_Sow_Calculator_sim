528f2976ec4db8cb98bc79a1aeb3ee79c508585e
hosh-farms <admin@hoshfarms.com>
2025-09-06 00:26:07 +0530

    Update sowcalcmonthly_withgraphs.py

--- sowcalcmonthly_withgraphs.py
+++ sowcalcmonthly_withgraphs.py
@@ -258,16 +258,16 @@ def sow_rotation_simulator(
     # Calculate number of years over realized period
     years_realized = (df_month.at[last_idx, 'Month'] - df_month.at[first_idx, 'Month'] + 1) / 12
     years = months / 12
-    realized_cagr = ((cumulative_cash_flow + total_capital_invested/ total_capital_invested + first_sale_cash_needed) ** (1 / years)) - 1
+    # realized_cagr = ((cumulative_cash_flow + total_capital_invested/ total_capital_invested + first_sale_cash_needed) ** (1 / years)) - 1
 
 
     
-    # CAGR on realized cash flows
-    # --- Realized CAGR on cash flows ---
-    # initial_outflow = first_sale_cash_needed if first_sale_cash_needed > 0 else 1  # avoid zero
-    # final_inflow = cumulative_cash_flow + initial_outflow  # total cash in hand including initial outflow
-    # years = months / 12
-    # realized_cagr = ((final_inflow / initial_outflow + total_capital) ** (1/years) - 1) * 100 if initial_outflow > 0 else 0
+    CAGR on realized cash flows
+    --- Realized CAGR on cash flows ---
+    initial_outflow = first_sale_cash_needed if first_sale_cash_needed > 0 else 1  # avoid zero
+    final_inflow = cumulative_cash_flow + initial_outflow  # total cash in hand including initial outflow
+    years = months / 12
+    realized_cagr = ((final_inflow / initial_outflow + total_capital) ** (1/years) - 1) * 100 if initial_outflow > 0 else 0
     return df_month, df_year, total_sow_cost, shed_cost, first_sale_cash_needed, total_pigs_sold, total_pigs_born, animals_left, cumulative_cash_flow, total_interest_paid, break_even_month, profit_after_break_even, average_monthly_profit, avg_profit_after_breakeven, total_crossings, roi_with_assets, realized_cagr, total_roi_pct
 # -------------------------------
 # Streamlit UI
